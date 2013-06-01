f19830c64fd2a8eb9207de11b31d654cd5c6cab3
王佳裕 <fakefish.wjy@gmail.com>
2013-06-01 10:44:22 +0800

    first commit

---
+++ models/post.js
@@ -0,0 +1,75 @@
+var mongodb = require('./db');
+
+function Post(name, title, post) {
+	this.name = name;
+	this.title = title;
+	this.post = post;
+}
+
+module.exports = Post;
+
+Post.prototype.save = function(callback) {
+	var date = new Date();
+
+	var time = {
+		date: date,
+		year: date.getFullYear(),
+		month: date.getFullYear()+"-"+(date.getMonth()+1),
+		day: date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate(),
+		minute: date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()
+	};
+
+	var post = {
+		name: this.name,
+		time: time,
+		title: this.title,
+		post: this.post,
+	};
+
+	mongodb.open(function(err, db) {
+		if (err) {
+			return callback(err);
+		}
+		db.collection('posts', function(err, collection) {
+			if(err) {
+				mongodb.close();
+				return callback(err);
+			}
+			collection.insert(post, {
+				safe: true
+			}, function(err, post) {
+				mongodb.close();
+				callback(null);
+			});
+		});
+	});
+};
+Post.get = function(name, callback) {//读取文章及其相关信息
+  //打开数据库
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    //读取 posts 集合
+    db.collection('posts', function(err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      var query = {};
+      if (name) {
+        query.name = name;
+      }
+      //根据 query 对象查询文章
+      collection.find(query).sort({
+        time: -1
+      }).toArray(function (err, docs) {
+        mongodb.close();
+        if (err) {
+          callback(err, null);//失败！返回 null
+        }
+        callback(null, docs);//成功！以数组形式返回查询的结果
+      });
+    });
+  });
+};
---
+++ routes/index.js
@@ -0,0 +1,140 @@
+var crypto = require('crypto'),
+		User = require('../models/user.js'),
+		Post = require('../models/post.js');
+/*
+ * GET home page.
+ */
+
+module.exports = function(app) {
+	
+	// index
+	app.get('/', function(req, res) {
+		Post.get(null, function(err, posts){
+			res.render('index', {
+				title: '主页',
+				user: req.session.user,
+				posts: posts,
+				success: req.flash('success').toString(),
+				error: req.flash('error').toString()
+			});
+		});
+		
+	});
+
+	// login
+	app.get('/login', checkNotLogin);
+	app.get('/login', function(req, res) {
+		res.render('login', { title: '主页'});
+	});
+
+	app.post('/login', checkNotLogin);
+	app.post('/login', function(req, res) {
+		var md5 = crypto.createHash('md5'),
+				password = md5.update(req.body.password).digest('hex');
+		User.get(req.body.name, function(err, user) {
+			if(!user) {
+				req.flash('error', '用户不存在！');
+				return res.redirect('/login');
+			}
+			if(user.password != password) {
+				req.flash('error', '密码错误！');
+				return res.redirect('/login');
+			}
+			req.session.user = user;
+			req.flash('success','登陆成功！');
+			res.redirect('/');
+		});
+	});
+
+	// reg
+	app.get('/reg', checkNotLogin);
+	app.get('/reg', function(req, res) {
+		res.render('reg', { title: '注册'});
+	});
+	app.post('/reg', checkNotLogin);
+	app.post('/reg', function(req,res){
+  var name = req.body.name,
+      password = req.body.password,
+      password_re = req.body['password-repeat'];
+  //检验用户两次输入的密码是否一致
+  if(password_re != password){
+    req.flash('error','两次输入的密码不一致!'); 
+    return res.redirect('/reg');
+  }
+  //生成密码的散列值
+  var md5 = crypto.createHash('md5'),
+      password = md5.update(req.body.password).digest('hex');
+  var newUser = new User({
+      name: req.body.name,
+      password: password,
+      email: req.body.email
+  });
+  //检查用户名是否已经存在 
+  User.get(newUser.name, function(err, user){
+    if(user){
+      err = '用户已存在!';
+    }
+    if(err){
+      req.flash('error', err);
+      return res.redirect('/reg');
+    }
+    //如果不存在则新增用户
+    newUser.save(function(err){
+      if(err){
+        req.flash('error',err);
+        return res.redirect('/reg');
+      }
+      req.session.user = newUser;//用户信息存入session
+      req.flash('success','注册成功!');
+      res.redirect('/');
+    });
+  });
+});
+
+	// post
+	app.get('/post', checkLogin);
+	app.get('/post',function(req,res){
+    res.render('post', {
+      title: '发表',
+      user: req.session.user,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    });
+  });
+	app.post('/post', checkLogin);
+  app.post('/post',function(req,res){
+  	var currentUser = req.session.user,
+  			post = new Post(currentUser.name, req.body.title, req.body.post);
+  	post.save(function(err){
+  		if(err) {
+  			req.flash('error', err);
+  			return res.redirect('/');
+  		}
+  		req.flash('success', '发布成功');
+  		res.redirect('/');
+  	});
+  });
+
+	// logout
+	app.get('/logout', function(req, res) {
+		req.session.user = null;
+		req.flash('success','登出成功');
+		res.redirect('/');
+	});
+};
+
+function checkLogin(req, res, next) {
+	if(!req.session.user) {
+		req.flash('error', '未登录！');
+		return res.redirect('/login');
+	}
+	next();
+}
+
+function checkNotLogin(req, res, next) {
+	if(req.session.user) {
+		req.flash('error','已登录！');
+		return res.redirect('/');
+	}
+	next();
+}
