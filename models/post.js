14eebc47e2025e8d77c7e6fadc06ab9e992bafed
王佳裕 <fakefish.wjy@gmail.com>
2013-06-06 15:49:24 +0800

    add tags

--- models/post.js
+++ models/post.js
@@ -1,50 +1,52 @@
 var mongodb = require('./db'),
-	markdown = require('markdown').markdown;
+  markdown = require('markdown').markdown;
 
-function Post(name, title, post) {
-	this.name = name;
-	this.title = title;
-	this.post = post;
+function Post(name, title, tags, post) {
+  this.name = name;
+  this.title = title;
+  this.tags = tags;
+  this.post = post;
 }
 
 module.exports = Post;
 
 Post.prototype.save = function(callback) {
-	var date = new Date();
+  var date = new Date();
 
-	var time = {
-		date: date,
-		year: date.getFullYear(),
-		month: date.getFullYear()+"-"+(date.getMonth()+1),
-		day: date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate(),
-		minute: date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()
-	};
+  var time = {
+    date: date,
+    year: date.getFullYear(),
+    month: date.getFullYear()+"-"+(date.getMonth()+1),
+    day: date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate(),
+    minute: date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()
+  };
 
-	var post = {
-		name: this.name,
-		time: time,
-		title: this.title,
-		post: this.post,
-		comments: []
-	};
+  var post = {
+    name: this.name,
+    time: time,
+    title: this.title,
+    tags: this.tags,
+    post: this.post,
+    comments: []
+  };
 
-	mongodb.open(function(err, db) {
-		if (err) {
-			return callback(err);
-		}
-		db.collection('posts', function(err, collection) {
-			if(err) {
-				mongodb.close();
-				return callback(err);
-			}
-			collection.insert(post, {
-				safe: true
-			}, function(err, post) {
-				mongodb.close();
-				callback(null);
-			});
-		});
-	});
+  mongodb.open(function(err, db) {
+    if (err) {
+      return callback(err);
+    }
+    db.collection('posts', function(err, collection) {
+      if(err) {
+        mongodb.close();
+        return callback(err);
+      }
+      collection.insert(post, {
+        safe: true
+      }, function(err, post) {
+        mongodb.close();
+        callback(null);
+      });
+    });
+  });
 };
 
 Post.getTen = function(name, page, callback) {
@@ -72,7 +74,7 @@ Post.getTen = function(name, page, callback) {
           callback(err, null);//失败！返回 null
         }
         docs.forEach(function(doc) {
-        	doc.post = markdown.toHTML(doc.post);
+          doc.post = markdown.toHTML(doc.post);
         });
         callback(null, docs);//成功！以数组形式返回查询的结果
       });
@@ -82,33 +84,100 @@ Post.getTen = function(name, page, callback) {
 
 
 Post.getOne = function(name, day, title, callback) {
-	mongodb.open(function(err, db) {
-		if (err) {
-			return callback(err);
-		}
-		db.collection('posts', function(err, collection) {
-			if (err) {
-				mongodb.close();
-				return callback(err);
-			}
-			collection.findOne({
-				"name":name,
-				"time.day":day,
-				"title":title
-			}, function(err, doc) {
-				mongodb.close();
-				if (err) {
-					callback(err, null);
-				}
-				// doc.post = markdown.toHTML(doc.post);
-				if(doc){
-				  doc.post = markdown.toHTML(doc.post);
-				  doc.comments.forEach(function(comment){
-				    comment.content = markdown.toHTML(comment.content);
-				  });
-				}
-				callback(null, doc);
-			});
-		});
-	});
+  mongodb.open(function(err, db) {
+    if (err) {
+      return callback(err);
+    }
+    db.collection('posts', function(err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      collection.findOne({
+        "name":name,
+        "time.day":day,
+        "title":title
+      }, function(err, doc) {
+        mongodb.close();
+        if (err) {
+          callback(err, null);
+        }
+        // doc.post = markdown.toHTML(doc.post);
+        if(doc){
+          doc.post = markdown.toHTML(doc.post);
+          doc.comments.forEach(function(comment){
+            comment.content = markdown.toHTML(comment.content);
+          });
+        }
+        callback(null, doc);
+      });
+    });
+  });
+};
+
+Post.getArchive = function(callback) {
+  mongodb.open(function(err, db) {
+    if(err) {
+      return callback(err);
+    }
+    db.collection('post', function(err, collection) {
+      if(err) {
+        mongodb.close();
+        return callback(err);
+      }
+      collection.find({}, {"name":1, "time":1,"title":1}).sort({
+        time:-1
+      }).toArray(function(err, docs) {
+        mongodb.close();
+        if(err) {
+          callback(err, null);
+        }
+        callback(null, docs);
+      });
+    });
+  });
+};  
+
+Post.getTags = function(callback) {
+  mongodb.open(function(err, db) {
+    if(err) {
+      return callback(err);
+    }
+    db.collection('posts', function(err, collection) {
+      if(err) {
+        mongodb.close();
+        return callback(err);
+      }
+      collection.distinct("tags.tag", function(err, docs) {
+        mongodb.close();
+        if(err) {
+          callback(err, null);
+        }
+        callback(null, docs);
+      });
+    });
+  });
+};
+
+Post.getTag = function(tag, callback) {
+  mongodb.open(function(err, db) {
+    if(err) {
+      return callback(err);
+    }
+    db.collection('posts', function(err, collection) {
+      if(err) {
+        mongodb.close();
+        return callback(err);
+      }
+      collection.find({"tags.tag":tag},{"name":1,"time":1,"title":1}).sort({
+        time:-1
+      }).toArray(function(err, docs) {
+        mongodb.close();
+        if(err) {
+          callback(err, null);
+        }
+        callback(null, docs);
+      });
+    });
+  });
 };
--- routes/index.js
+++ routes/index.js
@@ -112,7 +112,8 @@ module.exports = function(app) {
   app.post('/post', checkLogin);
   app.post('/post',function(req,res){
     var currentUser = req.session.user,
-        post = new Post(currentUser.name, req.body.title, req.body.post);
+        tags = [{"tag":req.body.tag1},{"tag":req.body.tag2},{"tag":req.body.tag3}],
+        post = new Post(currentUser.name, req.body.title, tags, req.body.post);
     post.save(function(err){
       if(err) {
         req.flash('error', err);
@@ -198,6 +199,54 @@ module.exports = function(app) {
 
   });
 
+  app.get('/archive', function(req, res) {
+    Post.getArchive(function(err, posts) {
+      if(err) {
+        req.flash('err', err);
+        return res.redirect('/');
+      }
+      res.render('archive', {
+        title: '存档',
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  });
+
+  app.get('/tags', function(req, res) {
+    Post.getTags(function(err, posts) {
+      if(err) {
+        req.flash('err', err);
+        return res.redirect('/');
+      }
+      res.render('tags', {
+        title: '标签',
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  });
+
+  app.get('/tags/:tag', function(req, res) {
+    Post.getTag(req.params.tag, function(err, posts) {
+      if(err){
+        req.flash('error', err);
+        return res.redirect('/');
+      }
+      res.render('tag', {
+        title: 'TAG:'+req.params.tag,
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  });
+  
 };
 
 function checkLogin(req, res, next) {
